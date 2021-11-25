f6c41ca6d9a4f1490848ad0e21258c83c52ccd4f
minhyukko <komhgogo@gmail.com>
2021-11-25 17:03:22 +0900

    Created : feature/minhyuk-signin

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views           import View
+from django.http            import JsonResponse
+from django.core.exceptions import ValidationError
+
+from users.models           import User
+from .validator             import is_blank, email_validate, password_validate
+
+class SignupView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            is_blank(data.get('email'), data.get('password'))
+            email_validate(data['email'])
+            password_validate(data['password'])
+            
+            if User.objects.filter(email = data['email']).exists():
+                raise ValidationError('DUPLICATED_EMAIL')
+            
+            User.objects.create(
+                name        = data['name'],
+                email       = data['email'],
+                password    = data['password'],
+                address     = data['address'],
+                information = data.get('information')
+            )
+            return JsonResponse({'message':'SUCCESS'},status=201)
+
+        except ValidationError as e:
+            return JsonResponse({'message': e.message},status=400)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
